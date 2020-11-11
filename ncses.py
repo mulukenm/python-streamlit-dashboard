605cdbab157bdd4f3b464bfff10db261325abe8b
Muluken <mmuche2013@gmail.com>
2020-11-10 20:28:09 -0500

    update

--- ncses.py
+++ ncses.py
@@ -31,10 +31,22 @@ select = st.sidebar.selectbox('Visualization type', ['Bar plot', 'Pie chart'], k
 race_count = data.groupby(["Race_and_Ethnicity"])['Number'].agg('sum')
 race_count = pd.DataFrame({'Race_and_Ethnicity':race_count.index, 'Number':race_count.values})
 if not st.sidebar.checkbox("Hide", True):
-    st.markdown("### Number of tweets by sentiment")
+    st.markdown("### Numbers by Race/Ethnicity")
     if select == 'Bar plot':
         fig = px.bar(race_count, x='Race_and_Ethnicity', y='Number', color='Race_and_Ethnicity', height=500)
         st.plotly_chart(fig)
     else:
         fig = px.pie(race_count, values='Number', names='Race_and_Ethnicity')
+        st.plotly_chart(fig)
+st.sidebar.markdown("### Number of Earned Doctorates by Sex")
+select = st.sidebar.selectbox('Visualization type', ['Bar plot', 'Pie chart'], key='1')
+race_count = data.groupby(["Sex"])['Number'].agg('sum')
+race_count = pd.DataFrame({'Sex':race_count.index, 'Number':race_count.values})
+if not st.sidebar.checkbox("Hide", True):
+    st.markdown("### Numbers by Sex")
+    if select == 'Bar plot':
+        fig = px.bar(race_count, x='Sex', y='Number', color='Sex', height=500)
+        st.plotly_chart(fig)
+    else:
+        fig = px.pie(race_count, values='Number', names='Sex')
         st.plotly_chart(fig)
