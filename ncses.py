0d6d863914078cecb51fd669d448f877a9a06d97
Muluken <mmuche2013@gmail.com>
2020-11-11 11:33:57 -0500

    update

--- ncses.py
+++ ncses.py
@@ -28,7 +28,8 @@ data = load_data()
 
 st.sidebar.markdown("### Number of Earned Doctorates")
 sex = st.sidebar.selectbox('Visualization type', ['Sunburst'])
-fig1 = px.sunburst(data, path=['Detailed_Fields', 'Year', 'Sex'], values='Number', height = 700, width = 700)
+fig1 = px.sunburst(data, path=['S&E_Fields','Broad_Fields', 'Detailed_Fields', 'Year', 'Sex'], values='Number', height = 700, width = 700)
+st.sidebar.checkbox("Hide", True):
 #fig1.update_layout(
     # title={
     #     'text': "Field of Study of Doctrate Recipients by Sex and Selected Years",
