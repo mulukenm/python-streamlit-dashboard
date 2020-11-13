4ca5b695461ae125b5b68f36e3d1d258b01c4239
mulukenm <mmuche2013@gmail.com>
2020-11-12 21:18:56 -0500

    update

--- ncses.py
+++ ncses.py
@@ -28,21 +28,23 @@ data = load_data()
 st.sidebar.markdown("### Earned Doctorate Recipients")
 select1 = st.sidebar.selectbox('Visualization type', ['Sunburst'])
 if not st.sidebar.checkbox("Close", True):
-    fig_1 = px.sunburst(data, path=['S&E_Fields','Broad_Fields', 'Detailed_Fields', 'Year', 'Sex'], values='Number', height = 800, width = 800)
-    #fig1.update_layout(
+    if select1 == 'Sunburst':
+        fig_1 = px.sunburst(data, path=['S&E_Fields','Broad_Fields', 'Detailed_Fields', 'Year', 'Sex'], values='Number', height = 800, width = 800)
+        #fig1.update_layout(
         # title={
         #     'text': "Field of Study of Doctorate Recipients by Sex and Selected Years",
         #     'y':0.94,
         #     'x':0.5,
         #     'xanchor': 'center',
         #     'yanchor': 'top'})
-    st.plotly_chart(fig_1)
+        st.plotly_chart(fig_1)
 
 st.sidebar.markdown("### Earned Doctorate Recipients by Sex and Race/Ethnicity")
 select2 = st.sidebar.selectbox('Visualization type', ['Barplot'], key='2')
 if not st.sidebar.checkbox("Hide", True, key='2'):
-    fig_1 = px.bar(data, x="Race_and_Ethnicity", y="Number", color="Sex", barmode="group")
-    st.plotly_chart(fig_2)
+    if select2 == 'Bar plot':
+        fig_2 = px.bar(data, x="Race_and_Ethnicity", y="Number", color="Sex", barmode="group")
+        st.plotly_chart(fig_2)
 
 
 st.sidebar.markdown("### Earned Doctorate Recipients by Race and Ethnicity")
@@ -51,7 +53,7 @@ race_count = data.groupby(["Race_and_Ethnicity"])['Number'].agg('sum')
 race_count = pd.DataFrame({'Race_and_Ethnicity':race_count.index, 'Number':race_count.values})
 if not st.sidebar.checkbox("Hide", True, key='3'):
     st.markdown("### Earned Doctorate Recipients by Race/Ethnicity")
-    if select == 'Bar plot':
+    if select3 == 'Bar plot':
         fig_3 = px.bar(race_count, x='Race_and_Ethnicity', y='Number', color='Number', height=500)
         st.plotly_chart(fig_3)
     else:
@@ -64,7 +66,7 @@ sex_count = data.groupby(["Sex"])['Number'].agg('sum')
 sex_count = pd.DataFrame({'Sex':sex_count.index, 'Number':sex_count.values})
 if not st.sidebar.checkbox("Hide", True, key='4'):
     st.markdown("### Earned Doctorate Recipients by Sex")
-    if select == 'Bar plot':
+    if select4 == 'Bar plot':
         fig_4 = px.bar(sex_count, x='Sex', y='Number', color='Number', height=500)
         st.plotly_chart(fig_4)
     else:
